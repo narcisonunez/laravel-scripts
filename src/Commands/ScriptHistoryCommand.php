eef05142947ca01459babc9466a9a1e21fe3f6e3
Narciso Nunez <narciso.arias21@gmail.com>
2021-03-07 20:27:33 -0400

    wip

--- src/Commands/ScriptHistoryCommand.php
+++ src/Commands/ScriptHistoryCommand.php
@@ -3,12 +3,16 @@
 namespace Narcisonunez\LaravelScripts\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemManager;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Narcisonunez\LaravelScripts\Models\ScriptRun;
+use PHPUnit\Util\Filesystem;
 use Symfony\Component\Console\Input\InputOption;
 
 class ScriptHistoryCommand extends Command
 {
-    public $signature = 'scripts:history {--script} {--limit}';
+    public $signature = 'scripts:history {--script=} {--limit=}';
 
     public $description = 'Print the history of scripts runs';
 
@@ -19,8 +23,7 @@ class ScriptHistoryCommand extends Command
 
         if ($scriptName && ! class_exists(config('scripts.base_path') . "\\$scriptName")) {
             $this->error('Class not found: ' . config('scripts.base_path') . "\\" . $scriptName);
-
-            return;
+            $scriptName = $this->showScriptOptions();
         }
 
         $historyQuery = ScriptRun::query()
@@ -32,7 +35,6 @@ class ScriptHistoryCommand extends Command
         }
 
         $history = $historyQuery->get(['id', 'script_name', 'status', 'message', 'runner_ip']);
-
         $this->table(
             ['ID', 'Script Name', 'Status', 'Message', 'Runner IP'],
             $history->toArray()
@@ -50,5 +52,24 @@ class ScriptHistoryCommand extends Command
             ['script', null, InputOption::VALUE_OPTIONAL, 'Show the history for this specific script'],
             ['limit', null, InputOption::VALUE_OPTIONAL, 'Number of rows returned. By default is 10.'],
         ];
+    }
+
+    /**
+     * Show available scripts to the user console
+     * @return array|string
+     */
+    private function showScriptOptions()
+    {
+        $files = collect(File::allFiles(app_path('Scripts')))->map(function($file){
+            return [
+                'filename' => Str::replaceFirst('.php', '',  $file->getFilename())
+            ];
+        })->values()->flatten()->toArray();
+        $this->newLine();
+        $this->info('Available commands');
+        return $this->choice(
+            'Pick one of the following commands. (Cmd + C to exit)',
+            $files
+        );
     }
 }

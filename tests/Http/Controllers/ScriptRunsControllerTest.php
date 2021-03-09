72e8a1bfeb8095fdd7cc8a25df45a6879e8bf932
Narciso Nunez <narciso.arias21@gmail.com>
2021-03-09 16:34:52 -0400

    wip

---
+++ tests/Http/Controllers/ScriptRunsControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Narcisonunez\LaravelScripts\Tests\Http\Controllers;
+
+use Narcisonunez\LaravelScripts\Database\Factories\ScriptRunFactory;
+use Narcisonunez\LaravelScripts\Models\ScriptRun;
+use Narcisonunez\LaravelScripts\Tests\TestCase;
+
+class ScriptRunsControllerTest extends TestCase
+{
+    /** @test */
+    public function it_should_load_the_history_page()
+    {
+        /** @var ScriptRun $scriptRun */
+        $scriptRun = ScriptRunFactory::new()->create();
+        $this->get('/scripts')
+            ->assertSeeText($scriptRun->script_name)
+            ->assertSeeText($scriptRun->status)
+            ->assertSeeText($scriptRun->description)
+            ->assertOk();
+    }
+}

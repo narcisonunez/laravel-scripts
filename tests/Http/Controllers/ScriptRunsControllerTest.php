1a36d19b95e80f22d45eae9f2efd4851b680f316
Narciso Nunez <narciso.arias21@gmail.com>
2021-03-10 09:26:34 -0400

    wip

--- tests/Http/Controllers/ScriptRunsControllerTest.php
+++ tests/Http/Controllers/ScriptRunsControllerTest.php
@@ -3,12 +3,14 @@
 
 namespace Narcisonunez\LaravelScripts\Tests\Http\Controllers;
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Narcisonunez\LaravelScripts\Database\Factories\ScriptRunFactory;
 use Narcisonunez\LaravelScripts\Models\ScriptRun;
 use Narcisonunez\LaravelScripts\Tests\TestCase;
 
 class ScriptRunsControllerTest extends TestCase
 {
+    use WithoutMiddleware;
     /** @test */
     public function it_should_load_the_history_page()
     {

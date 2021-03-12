b97f794efa54da2d10898e1256345216aac64e3b
Narciso Nunez <narciso.arias21@gmail.com>
2021-03-12 11:56:07 -0400

    wip

--- src/Services/ScriptDependencyInput.php
+++ src/Services/ScriptDependencyInput.php
@@ -81,7 +81,7 @@ class ScriptDependencyInput
      */
     public function label() : string
     {
-        $label = cameltotitle($this->name);
+        $label = Str::title(implode(' ', preg_split('/(?=[A-Z])/', $this->name)));
 
         return Str::replaceLast('?', '', $label);
     }
